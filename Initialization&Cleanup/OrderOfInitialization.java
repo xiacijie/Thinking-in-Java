dffbe25ed5e5c957ac1bbee9fc7f410cde4902ac
xiacijie <cijie@ualberta.ca>
2018-06-04 00:02:57 +0800

    P185

--- Initialization&Cleanup/OrderOfInitialization.java
+++ Initialization&Cleanup/OrderOfInitialization.java
@@ -1,6 +1,10 @@
 //Demonstrates initialization order
 
 public class OrderOfInitialization {
+    public static void main(String[] args){
+        House h = new House();
+        h.f();
+    }
 
 }
 
@@ -13,6 +17,15 @@ class Window{
 }
 
 class House{
-    Window w1 = new Window(1);
+    Window w1 = new Window(1);// Before constructor
+    House(){
+        //Show that we are in the constructor
+        System.out.println("House()");
+        w3 = new Window(33);//reinitialize w3
+    }
+
+    Window w2 = new Window(2); //After constructor
+    void f(){System.out.println("f()");}
+    Window w3 = new Window(3); // At end
 }
 
