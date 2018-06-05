98bc00a5d49446f1a56f350d6445725949a16965
xiacijie <cijie@ualberta.ca>
2018-06-05 23:42:53 +0800

    P195

---
+++ Initialization&Cleanup/ArrayNew.java
@@ -0,0 +1,12 @@
+//creating arrays with new
+import java.util.*;
+
+public class ArrayNew {
+    public static void main(String[] args){
+        int[] a;
+        Random rand = new Random(47);
+        a = new int[rand.nextInt(20)];
+        System.out.println("length of a = "+a.length);
+        System.out.println(Arrays.toString(a));
+    }
+}
