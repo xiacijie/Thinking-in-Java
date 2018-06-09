cd8db0ca5d4400025247ebc16223e7fdb8755718
xiacijie <cijie@ualberta.ca>
2018-06-09 21:04:35 +0800

    p196

---
+++ Initialization&Cleanup/ArrayClassObj.java
@@ -0,0 +1,14 @@
+//creating an array of nonprimitive objects
+import java.util.*;
+public class ArrayClassObj {
+    public static void main(String[] args){
+        Random rand = new Random(47);
+        Integer[] a = new Integer[rand.nextInt(20)];
+        System.out.println("length of a = "+a.length);
+        for (int i =0;i<a.length;i++){
+            a[i] = rand.nextInt(500); //autoboxing
+        }
+        System.out.println(Arrays.toString(a));
+
+    }
+}
