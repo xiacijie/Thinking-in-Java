e757c1726c2c44e603d07277b3c64562b15bdd41
xiacijie <cijie@ualberta.ca>
2018-06-09 23:43:49 +0800

    p203

---
+++ Initialization&Cleanup/Varaible_argument_lists/OverloadingVarargs.java
@@ -0,0 +1,34 @@
+package Varaible_argument_lists;
+
+public class OverloadingVarargs {
+    static void f(Character... args){
+        System.out.print("first");
+        for (Character c: args)
+            System.out.print(" "+c);
+        System.out.println();
+    }
+
+
+    static void f(Integer... args){
+        System.out.print("second");
+        for (Integer i : args){
+            System.out.print(" "+i);
+        }
+        System.out.println();
+    }
+
+
+    static void f(Long... args){
+        System.out.println("third");
+    }
+
+
+    public  static void main(String[] args){
+        f('a','b','c');
+        f(1);
+        f(2,1);
+        f(0);
+        f(0L);
+        //f() won't compile !
+    }
+}
