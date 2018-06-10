360d81d68c12eb291d2893d428040d2646650c62
xiacijie <cijie@ualberta.ca>
2018-06-10 23:19:17 +0800

    p225

---
+++ AccessControl/access/IceCream.java
@@ -0,0 +1,17 @@
+//Demonstrates private keyword
+package access;
+
+public class IceCream {
+    public static void main(String[] args){
+//         Sundae x = new Sundae();
+        Sundae x = Sundae.makeASundae();
+    }
+}
+
+
+class Sundae{
+    private Sundae(){}
+    static Sundae makeASundae(){
+        return new Sundae();
+    }
+}
