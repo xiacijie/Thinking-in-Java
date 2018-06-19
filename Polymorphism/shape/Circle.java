d0d20d51aa167e6944923bd9e171ed795c3f6a46
xiacijie <cijie@ualberta.ca>
2018-06-19 15:54:35 +0800

    p283

---
+++ Polymorphism/shape/Circle.java
@@ -0,0 +1,12 @@
+package shape;
+
+public class Circle extends Shape {
+    public void draw(){
+        System.out.println("Circle.draw()");
+
+    }
+
+    public void erase(){
+        System.out.println("Circle.erase()");
+    }
+}
---
+++ Polymorphism/shape/Shape.java
@@ -0,0 +1,6 @@
+package shape;
+
+public class Shape {
+    public void draw(){}
+    public void erase(){}
+}
