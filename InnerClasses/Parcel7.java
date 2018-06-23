74f459bd5dc484d1d9efa7ac7f906f8ea37d9189
xiacijie <cijie@ualberta.ca>
2018-06-23 22:33:43 +0800

    p356

---
+++ InnerClasses/Parcel7.java
@@ -0,0 +1,19 @@
+//Returning an instance of an anonymous inner class
+
+public class Parcel7 {
+    public Contents contents(){
+        return new Contents() { //insert a class definition
+            private int i = 11;
+            public int value(){
+                return i;
+            }
+
+        }; // Semicolon required in this case
+    }
+
+    public static void main(String[] args){
+        Parcel7 p =  new Parcel7();
+        Contents c = p.contents();
+    }
+
+}
