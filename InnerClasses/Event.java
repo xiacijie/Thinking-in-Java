c07f1905dccd798a860e512be8f10f1a02492345
xiacijie <cijie@ualberta.ca>
2018-07-03 22:27:16 +0800

    p376

---
+++ InnerClasses/Event.java
@@ -0,0 +1,21 @@
+//The common method for any control event
+
+
+public abstract class Event {
+    private long eventTime;
+    protected final long delayTime;
+    public Event(long delayTime){
+        this.delayTime = delayTime;
+        start();
+    }
+
+    public void start(){ //allows restarting
+        eventTime = System.nanoTime() + delayTime;
+    }
+
+    public boolean ready(){
+        return System.nanoTime() >= eventTime;
+    }
+
+    public abstract void action();
+}
